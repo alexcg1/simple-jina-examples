e76bea29aa46e623fedf1b2fb79940638ccecd6d
Alex C-G <alexcg@outlook.com>
2021-08-23 16:52:46 +0200

    feat: metadata, fix model name, clean up

--- image_search/executors.py
+++ image_search/executors.py
@@ -1,8 +1,11 @@
 from jina import Executor, requests
+import os
 
 
-class ToBlobExecutor(Executor):
+class ProcessFile(Executor):
     @requests
     def foo(self, docs, **kwargs):
         for doc in docs:
+            doc.tags["uri"] = doc.uri
+            doc.tags["uri_absolute"] = os.path.abspath(doc.uri)
             doc.convert_image_uri_to_blob()
