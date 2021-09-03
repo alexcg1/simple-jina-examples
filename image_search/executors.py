8bb89471d30bfbf410d681d3696f87e9a2150863
Alex C-G <alexcg@outlook.com>
2021-09-03 11:25:01 +0200

    refactor: clearer fn name

--- image_search/executors.py
+++ image_search/executors.py
@@ -4,7 +4,7 @@ import os
 
 class UriToBlob(Executor):
     @requests
-    def foo(self, docs, **kwargs):
+    def uri_to_blob(self, docs, **kwargs):
         for doc in docs:
             doc.tags["uri"] = doc.uri
             doc.tags["uri_absolute"] = os.path.abspath(doc.uri)
